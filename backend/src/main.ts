7d2b97f24bab9de5e346449f497b68b6872fe989
Aurélien <robineau.aure@gmail.com>
2021-06-08 09:56:24 +0200

    Change server port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	await app.listen(3000)
+	await app.listen(60000)
+	console.log(`Listening on http://localhost:60000`);
 }
 bootstrap()
