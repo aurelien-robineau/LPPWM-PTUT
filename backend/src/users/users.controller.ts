7fb4b069d532fdb0cbb93a959cee27205a41dcc1
Aurélien <robineau.aure@gmail.com>
2021-06-10 12:25:53 +0200

    Add v1 to endpoint url

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,7 +1,7 @@
 import { Controller } from '@nestjs/common'
 import { UsersService } from './users.service';
 
-@Controller('users')
+@Controller('v1/users')
 export class UsersController {
 	constructor(
 		private readonly service: UsersService
