66a6981638269d4a665431f415ac68a6d36de5a1
Hippolyte Soulier <hi.soulier@gmail.com>
2021-06-14 14:17:16 +0200

    ✨ Feat: Signin Page

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,7 +1,73 @@
-import React from "react"
+import { useState, useReducer } from "react"
+
+const formReducer = (state: any, event: any) => {
+	return {
+		...state,
+		[event.name]: event.value,
+	}
+}
 
 const Signin = () => {
-	return <div>Signin</div>
+	const [formData, setFormData] = useReducer(formReducer, {})
+	const [submitting, setSubmitting] = useState(false)
+
+	const handleSubmit = (event: any) => {
+		event.preventDefault()
+		setSubmitting(true)
+
+		setTimeout(() => {
+			setSubmitting(false)
+		}, 3000)
+	}
+
+	const handleChange = (event: any) => {
+		setFormData({
+			name: event.target.name,
+			value: event.target.value,
+		})
+	}
+	return (
+		<div className="signin-page">
+			<div className="container">
+				<h1>S’inscrire sur Enyu</h1>
+				{submitting && (
+					<div>
+						You are submitting the following:
+						<ul>
+							{Object.entries(formData).map(
+								([name, value]: Array<any>) => (
+									<li key={name}>
+										<strong>{name}</strong>:
+										{value.toString()}
+									</li>
+								)
+							)}
+						</ul>
+					</div>
+				)}
+				<form onSubmit={handleSubmit}>
+					<label htmlFor="email">Email *</label>
+					<input type="email" id="email" onChange={handleChange} />
+					<label htmlFor="idEnedis">
+						Identifiant client Enedis *
+					</label>
+					<input type="text" id="idEnedis" onChange={handleChange} />
+					<label htmlFor="password">Mot de passe *</label>
+					<input
+						type="password"
+						id="password"
+						onChange={handleChange}
+					/>
+					<button type="submit">S'inscrire</button>
+				</form>
+				<div className="create-account">
+					Déjà un compte ? <a href="/login">Connectez-vous</a>
+				</div>
+				<div className="plant">Plante/logo à mettre</div>
+				<span>* Champs obligatoires</span>
+			</div>
+		</div>
+	)
 }
 
 export default Signin
