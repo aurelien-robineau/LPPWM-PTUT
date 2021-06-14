f6c47602daa831f6d57600f1f2cff4add9f2d84c
Hippolyte Soulier <hi.soulier@gmail.com>
2021-06-14 14:47:37 +0200

    🌙 Feat: Dark Theme

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,7 +1,22 @@
-import React from "react"
+import { useState, useEffect } from "react"
 
 const Dashboard = () => {
-	return <div>Dashboard</div>
+	const [darkMode, setDarkMode] = useState(
+		!!localStorage.getItem("DARK_THEME") || false
+	)
+	useEffect(() => {
+		localStorage.setItem("DARK_THEME", darkMode.toString())
+	}, [darkMode])
+
+
+	return (
+		<div>
+			Dashboard
+			<button onClick={() => setDarkMode(darkMode ? false : true)}>
+				Switch theme
+			</button>
+		</div>
+	)
 }
 
 export default Dashboard
