98756b913f8e302c88aeadb771d514734ec9e3ce
Lise Phan <lisephan2000@gmail.com>
2021-06-17 10:59:37 +0200

    temp commit Lise

--- frontend/src/components/ConsumpMonitoring/index.tsx
+++ frontend/src/components/ConsumpMonitoring/index.tsx
@@ -1,7 +1,80 @@
+import IconArrowUp from "../Icons/ArrowUp"
+import IconArrowDown from "../Icons/ArrowDown"
+
 const ConsumpMonitoring = () => {
 	return (
 		<section>
 			<h2>Cartes de conso + plante</h2>
+
+			{/* if conso hier > conso avant-hier */}
+			<div className="day-conso-card">
+				<div>
+					<span className="day-data">3</span>kw
+					<IconArrowUp size={20} />
+				</div>
+				<div>
+					<p>de plus que la veille</p>
+				</div>
+			</div>
+
+			{/* else */}
+			<div className="day-conso-card">
+				<div>
+					<span className="day-data">2</span>kw
+					<IconArrowDown size={20} />
+				</div>
+				<div>
+					<p>de moins que la veille</p>
+				</div>
+			</div>
+
+
+			<div className="conso-row">
+				{/* if conso semaine > conso avant-semaine */}
+				<div className="week-conso-card">
+					<div>
+						<span className="week-data">17</span>kw
+						<IconArrowUp size={14} />
+					</div>
+					<div>
+						<p>de plus que la semaine dernière</p>
+					</div>
+				</div>
+
+				{/* else */}
+				<div className="week-conso-card">
+					<div>
+						<span className="week-data">17</span>kw
+						<IconArrowDown size={14} />
+					</div>
+					<div>
+						<p>de moins que la semaine dernière</p>
+					</div>
+				</div>
+
+
+				{/* if conso mois > conso avant-mois */}
+				<div className="month-conso-card">
+					<div>
+						<span className="month-data">10</span>kw
+						<IconArrowUp size={14} />
+					</div>
+					<div>
+						<p>de plus que le mois dernier</p>
+					</div>
+				</div>
+
+				{/* else */}
+				<div className="month-conso-card">
+					<div>
+						<span className="month-data">10</span>kw
+						<IconArrowDown size={14} />
+					</div>
+					<div>
+						<p>de moins que le mois dernier</p>
+					</div>
+				</div>
+			</div>
 		</section>
 	)
 }
--- frontend/src/components/Tracker/index.tsx
+++ frontend/src/components/Tracker/index.tsx
@@ -1,11 +1,57 @@
 import React from "react"
+import { PieChart, Pie, Sector, Cell } from "recharts";
+
+
+// const Tracker = () => {
+// 	return (
+
+// 		<div className="tracker-section">
+
+// 			<h2>Mon objectif de consommation</h2>
+
+// 		</div>
+// 	)
+// }
+
+// export default Tracker
+
+
+const data = [{ name: "Group A", value: 20 },
+{ name: "Group B", value: 80 },];
+
+
+const COLORS = ["green", "transparent"];
+
 
 const Tracker = () => {
 	return (
-		<div className="tracker">
-			<div className="tracker__container">
-				<h2>Mon objectif de consommation</h2>				
-			</div>
+
+		<div className="tracker-section">
+
+			<h2>Mon objectif de consommation</h2>
+
+			<PieChart width={375} height={300}>
+				<Pie
+					data={data}
+					cx={100}
+					cy={100}
+					innerRadius={60}
+					outerRadius={80}
+					fill="none"
+					stroke="none"
+					paddingAngle={0}
+					startAngle={90}
+					endAngle={450}
+					dataKey="value"
+				>
+					{data.map((entry, index) => (
+						<Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+					))}
+				</Pie>
+
+			</PieChart>
+
+
 		</div>
 	)
 }
