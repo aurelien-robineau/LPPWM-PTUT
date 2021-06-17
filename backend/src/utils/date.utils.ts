5bf6fbc28f3562c1a15e12a80a9b1f4eb8234b74
Aurélien <robineau.aure@gmail.com>
2021-06-17 10:26:54 +0200

    Fix bug

--- backend/src/utils/date.utils.ts
+++ backend/src/utils/date.utils.ts
@@ -2,16 +2,12 @@ export const getDayOnlyFromDate = (date: Date): string => {
 	return date.toISOString().split('T')[0]
 }
 
-export const removeTimeFromDate = (date: Date): Date => {
-	return new Date(
-		date.getUTCFullYear(),
-		date.getUTCMonth(),
-		date.getUTCDate()
-	)
-}
-
 export const addDaysToDate = (date: Date, numberOfDays: number): Date => {
 	return new Date(date.getTime() + 24 * numberOfDays * 3600 * 1000)
+}
+
+export const removeTimeFromDate = (date: Date): Date => {
+	return new Date(getDayOnlyFromDate(date))
 }
 
 export const getNumberOfDaysBetweenDates = (date1: Date, date2: Date): number => {
@@ -28,14 +24,21 @@ export const getFirstDateOfTheDateWeek = (date: Date): Date => {
 export const getDateWeekBounds = (date: Date): { start: Date, end: Date } => {
 	const start = getFirstDateOfTheDateWeek(date)
 	return {
-		start: start,
-		end: addDaysToDate(start, 6)
+		start: removeTimeFromDate(start),
+		end: removeTimeFromDate(addDaysToDate(start, 6))
 	}
 }
 
 export const getDateMonthBounds = (date: Date): { start: Date, end: Date } => {
+	const startDate = new Date(date)
+	startDate.setDate(1)
+
+	const endDate = new Date(date)
+	endDate.setMonth(date.getMonth() + 1)
+	endDate.setDate(0)
+
 	return {
-		start: new Date(date.getFullYear(), date.getMonth(), 1),
-		end: new Date(date.getFullYear(), date.getMonth() + 1, 0)
+		start: removeTimeFromDate(startDate),
+		end: removeTimeFromDate(endDate)
 	}
 }
